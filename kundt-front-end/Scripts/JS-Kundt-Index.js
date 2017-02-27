a3e8e0db9cb5ee4a87b1e7c42916d858fc9ff68d
Dark Hindi <dark.hindi@gmx.at>
2017-02-27 14:17:34 +0100

    Datepicker Update
    Datumsanzeige geändert auf Deutsch von U.S.
    
    Update der anzeige, von 1 auf 2 monate
    
    Deutsche sprache update

--- kundt-front-end/Scripts/JS-Kundt-Index.js
+++ kundt-front-end/Scripts/JS-Kundt-Index.js
@@ -14,19 +14,78 @@
 
 // Hier evtl Variablen anlegen für das Datum von/bis zur weitergabe für Step 1
 var $j = jQuery.noConflict();
-$j("#date_von").datepicker({    
-});
+$j("#date_von").datepicker({
+    numberOfMonths: 2,
+    showButtonPanel: true,
+    altField: "#datepicker_input",
+    dateFormat: "dd.mm.yy",
+    currentText: 'heute', currentStatus: '',
+    todayText: 'heute', todayStatus: '',
+    clearText: '-', clearStatus: '',
+    closeText: 'schließen', closeStatus: '',
+    monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+},
+$j.datepicker.regional['de-AT']);
 
 
-$j("#date_bis").datepicker({    
-});
+
+$j("#date_bis").datepicker({
+    numberOfMonths: 2,
+    showButtonPanel: true,
+    altField: "#datepicker_input",
+    dateFormat: "dd.mm.yy",
+    currentText: 'heute', currentStatus: '',
+    todayText: 'heute', todayStatus: '',
+    clearText: '-', clearStatus: '',
+    closeText: 'schließen', closeStatus: '',
+    monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+}, $j.datepicker.regional['de-AT']);
+
 
 $j("#date_von_mobile").datepicker({
-});
+    dateFormat: "dd.mm.yy",
+    currentText: 'heute', currentStatus: '',
+    todayText: 'heute', todayStatus: '',
+    clearText: '-', clearStatus: '',
+    closeText: 'schließen', closeStatus: '',
+    monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+}, $j.datepicker.regional['de-AT']);
+
 
 
 $j("#date_bis_mobile").datepicker({
-});
+    dateFormat: "dd.mm.yy",
+    currentText: 'heute', currentStatus: '',
+    todayText: 'heute', todayStatus: '',
+    clearText: '-', clearStatus: '',
+    closeText: 'schließen', closeStatus: '',
+    monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+}, $j.datepicker.regional['de-AT']);
+
 
 
 
